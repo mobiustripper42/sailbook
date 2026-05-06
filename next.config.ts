8d3f380d500d2556eb9ff9ace5f5e33b227b68e1
mobiustripper42 <mobius5kcrypto@gmail.com>
2026-05-06 12:42:54 +0000

    changes for mill-dev

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.50.202', 'sailbook-dev', '100.118.147.49'],
+  allowedDevOrigins: ['192.168.50.202', 'mill-dev', '100.118.147.49'],
   turbopack: {
     root: __dirname,
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000', '192.168.50.202', 'sailbook-dev:3000', '100.118.147.49:3000', '*.app.github.dev'],
+      allowedOrigins: ['localhost:3000', '192.168.50.202', 'mill-dev:3000', '100.118.147.49:3000', '*.app.github.dev'],
     },
   },
 };
